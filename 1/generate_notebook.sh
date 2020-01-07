2cf91e2f92bc5cd98816c8d926b0dce8ec40f64b
Martin Müller <martin.mathias.mueller@gmail.com>
2020-01-07 15:08:00 +0100

    start with task 1

---
+++ 1/generate_notebook.sh
@@ -0,0 +1,2 @@
+jupytext main.py --to notebook -o notebooks/task_1.ipynb
+jupytext main.py --to markdown -o notebooks/task_1.md
---
+++ 1/main.py
@@ -0,0 +1,16 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# # Task 1: Finetuning a GPT-2 language model
