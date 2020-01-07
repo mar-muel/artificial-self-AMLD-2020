c534d500720325347e20055aef3186a710aeea45
Martin Müller <martin.mathias.mueller@gmail.com>
2020-01-07 15:55:40 +0100

    add requirements and code to approach 1

--- 1/main.py
+++ 1/main.py
@@ -14,3 +14,13 @@
 # ---
 
 # # Task 1: Finetuning a GPT-2 language model
+
+# We will learn
+# * What it means to fine-tune a language model
+# * How to control the text generation process
+# * What are the limits of text generation
+# Note that we will ignore the complexity of the model as well as the mechanics of the fine-tuning process in this task (more of this at a later stage!).
+
+%tensorflow_version 1.x
+!pip install -r requirements.txt
+import gpt_2_simple as gpt2
