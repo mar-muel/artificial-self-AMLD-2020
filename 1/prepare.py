c8815c43b7ca3a45ba1aa2e78e9efb3e3a327f2a
Manuel Schneider <manuel.schneider@hest.ethz.ch>
2020-01-22 12:52:16 +0100

    added option to include param info in filename for prepare script task 1

--- 1/prepare.py
+++ 1/prepare.py
@@ -10,6 +10,8 @@ parser = ArgumentParser()
 parser.add_argument(dest='type', type=str, default='all', help="Which data type to convert? [all|tweets|chess|music|shakespeare|javascript|typescript|json|html]")
 parser.add_argument('--data-dir', '-d', dest='data_dir', type=str, default='../datasets/', help="Path to the directory containing the raw data.")
 parser.add_argument('--output-dir', '-o', dest='output_dir', type=str, default='./data/', help="Path to the output directory.")
+parser.add_argument('--short-filename', '-s', dest='short_filename', type=str, default='false', help="Does not include parameter info in filename.")
+parser.add_argument('--postfix', '-p', dest='postfix', type=str, default='', help="Postfix is appended to the filename stem before the suffix and parameter info (if applicable).")
 parser.add_argument('--num-samples', '-n', dest='num_samples', type=int, default=1000, help="Max number of samples to be exported.")
 parser.add_argument('--max-length', dest='max_length', type=int, default=2000, help="Max length of a sample.")
 parser.add_argument('--min-length', dest='min_length', type=int, default=10, help="Min length of a sample.")
@@ -25,12 +27,26 @@ if args.preserve_form == 'true':
 if args.preserve_lines == 'false':
     args.preserve_form = 'false'
 
+# set postfix for output files if short-filename is false
+if args.postfix != '':
+    args.postfix = '_' + args.postfix
+if args.short_filename == 'false':
+    args.postfix += f'_n{args.num_samples}_min{args.min_length}_max{args.max_length}'
+    if args.preserve_lines == 'false':
+        args.postfix += '_nolines'
+    else:
+        args.postfix += '_lines'
+    if args.preserve_form == 'false':
+        args.postfix += '_noform'
+    else:
+        args.postfix += '_form'
+
 # print arguments to show values in use
 print(args)
 
 # helper to use code samples in zip file
 def process_zip(name, regs, args):
-    with open(os.path.join(args.output_dir, name + '.txt'), 'w+') as fh:
+    with open(os.path.join(args.output_dir, name + args.postfix + '.txt'), 'w+') as fh:
         with zipfile.ZipFile(os.path.join(args.data_dir, name + '.zip'), 'r') as z:
             cnt = 0
             for entry in z.namelist():
@@ -60,13 +76,13 @@ if args.type in ['all','tweets']: # parse trump tweets
     df['length'] = df.text.apply(len)
     filter = (df.text>'2017')&(df.text.str.startswith('RT')==False)&(df.length>args.min_length)
     df = df[filter]
-    df.sample(args.num_samples).text.to_csv(os.path.join(args.output_dir, 'tweets.txt'), index=False, header=False, quoting=csv.QUOTE_NONE, escapechar="\\", sep="\\")
+    df.sample(args.num_samples).text.to_csv(os.path.join(args.output_dir, 'tweets' + args.postfix + '.txt'), index=False, header=False, quoting=csv.QUOTE_NONE, escapechar="\\", sep="\\")
     print('preparing tweet data set done.')
 
 # dataset from: https://www.ficsgames.org/download.html | year: 2019, month: whole year, type: Standard (average rating > 2000)
 if args.type in ['all','chess']: # parse chess games
     print('prepare chess data set...')
-    with open(os.path.join(args.output_dir, 'chess.txt'),'w+') as fh:
+    with open(os.path.join(args.output_dir, 'chess' + args.postfix + '.txt'),'w+') as fh:
         with open(os.path.join(args.data_dir, 'ficsgamesdb_2019_standard2000_nomovetimes_110541.pgn')) as fp:
            line = fp.readline()
            cnt = 0
@@ -80,7 +96,7 @@ if args.type in ['all','chess']: # parse chess games
 # dataset from: https://www.kaggle.com/raj5287/abc-notation-of-tunes/version/3
 if args.type in ['all','music']: # parse abc songs
     print('prepare music data set...')
-    with open(os.path.join(args.output_dir, 'music.txt'),'w+') as fh:
+    with open(os.path.join(args.output_dir, 'music' + args.postfix + '.txt'),'w+') as fh:
         with open(os.path.join(args.data_dir, 'abc_notation_songs.txt')) as fp:
             line = fp.readline()
             cnt = 0
@@ -104,7 +120,7 @@ if args.type in ['all','shakespeare']: # parse shakespeare plays
     df = pd.read_csv(os.path.join(args.data_dir, 'shakespeare_data.csv'))
     if args.preserve_lines == 'false':
         df = df[df.Player!=''].groupby(['Play','PlayerLinenumber'],as_index=False).agg(' '.join)
-    df.sample(args.num_samples).PlayerLine.to_csv(os.path.join(args.output_dir, 'shakespeare.txt'), index=False, header=False, quoting=csv.QUOTE_NONE, escapechar="\\", sep="\\")
+    df.sample(args.num_samples).PlayerLine.to_csv(os.path.join(args.output_dir, 'shakespeare' + args.postfix + '.txt'), index=False, header=False, quoting=csv.QUOTE_NONE, escapechar="\\", sep="\\")
     print('preparing shakespeare data set done.')
 
 # dataset from: javascript files from https://www.sri.inf.ethz.ch/js150
@@ -143,7 +159,7 @@ if args.type in ['all','json']: # parse json files
 # dataset from: https://www.kaggle.com/zavadskyy/lots-of-code, https://gist.github.com/VladislavZavadskyy/e31ab07b03a5c22b11982c49669a400b
 if args.type in ['all','html']: # parse html
     print('prepare html data set...')
-    with open(os.path.join(args.output_dir, 'html.txt'),'w+') as fh:
+    with open(os.path.join(args.output_dir, 'html' + args.postfix + '.txt'),'w+') as fh:
         with open(os.path.join(args.data_dir, 'html-dataset.txt')) as fp:
             data = fp.read()
             data = data.replace('<!DOCTYPE html>','\n<!DOCTYPE html>')
